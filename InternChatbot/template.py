ff6b8c69cee80c51df38f49fb5ce43843095fb10
Michelle <22026985@myrp.edu.sg>
2025-01-04 09:18:26 +0800

    c

--- InternChatbot/template.py
+++ InternChatbot/template.py
@@ -5,7 +5,6 @@ HTML_TEMPLATE = '''<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="ngrok-skip-browser-warning" content="true">
     <title>NCS Internship AI Chatbot</title>
     <style>
         :root[data-theme="light"] {
